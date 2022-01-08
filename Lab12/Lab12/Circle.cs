591bdfdfe9147de14e1cd656a28c7a05b15c93a3
kvo-52 <kvo.52@mail.ru>
2022-01-08 11:29:41 +0300

    Lab12

---
+++ Lab12/Lab12/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    public class Circle
+    {
+        public static double len;
+        public static double sq;
+
+        public static void Length(double radius)
+        {
+            len = 2 * Math.PI * radius;
+            Console.WriteLine("Длина окружности L = {0:00}", len);
+        }
+        public static void Square(double radius)
+        {
+            sq = Math.PI * radius * radius;
+            Console.WriteLine("Площадь круга S = {0:00}", sq);
+        }
+        public static void Point(double x, double y, double radius)
+        {
+            if (radius >= Math.Sqrt(x * x + y * y))
+            {
+                Console.WriteLine("Точка находится внутри круга");
+            }
+            else
+            {
+                Console.WriteLine("Точка находится за пределами круга");
+            }
+        }
+    }
+}
--- Lab12/Lab12/Program.cs
+++ Lab12/Lab12/Program.cs
@@ -29,31 +29,5 @@ namespace Lab12
             Console.ReadKey();
         }
     }
-    public static class Circle
-    {
-        public static double len;
-        public static double sq;
-
-        public static void Length(double radius)
-        {
-            len = 2 * Math.PI * radius;
-            Console.WriteLine("Длина окружности L = {0:00}", len);
-        }
-        public static void Square(double radius)
-        {
-            sq = Math.PI * radius * radius;
-            Console.WriteLine("Площадь круга S = {0:00}", sq);
-        }
-        public static void Point(double x, double y, double radius)
-        {
-            if (radius >= Math.Sqrt(x * x + y * y))
-            {
-                Console.WriteLine("Точка находится внутри круга");
-            }
-            else
-            {
-                Console.WriteLine("Точка находится за пределами круга");
-            }
-        }
-    }
+    
 }
